f0765379e9bd761cef0600ea8f8c936c7b292e59
anaakisawa <ana.akisawa@gmail.com>
2020-04-16 10:58:09 -0300

    terminada lição do nil

--- koans/about_nil.rb
+++ koans/about_nil.rb
@@ -2,7 +2,7 @@ require_relative 'edgecase'
 
 class AboutNil < EdgeCase::Koan
   def test_nil_is_an_object
-    assert nil.is_a?(Object), "Unlike NULL in other languages"
+    assert nil.is_a?(Object) == true, "Unlike NULL in other languages"
   end
 
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
@@ -18,7 +18,7 @@ class AboutNil < EdgeCase::Koan
     #  in a sandbox and catching the error class into the exception
     #  variable.  
     #
-    exception = assert_raise(___) do
+    exception = assert_raise(NoMethodError) do
       nil.some_method_nil_doesnt_know_about
     end
     
@@ -26,13 +26,13 @@ class AboutNil < EdgeCase::Koan
     #  What is the error message itself? What substring or pattern could 
     #  you test against in order to have a good idea what the string is?
     #  
-    assert_match /__/, exception.message
+    assert_match /undefined method `some_method_nil_doesnt_know_about' for nil:NilClass/, exception.message
   end
 
   def test_nil_has_a_few_methods_defined_on_it
-    assert_equal __, nil.nil?
-    assert_equal __, nil.to_s
-    assert_equal __, nil.inspect
+    assert_equal true, nil.nil?
+    assert_equal "", nil.to_s
+    assert_equal "nil", nil.inspect
 
     # THINK ABOUT IT:
     #
