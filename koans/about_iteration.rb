5308dd99ea5450456f2d6f6577b7998316eb1a5a
anaakisawa <ana.akisawa@gmail.com>
2020-04-22 10:40:16 -0300

    terminada lição sobre arrays iterarion

--- koans/about_iteration.rb
+++ koans/about_iteration.rb
@@ -62,13 +62,11 @@ class AboutIteration < EdgeCase::Koan
   end
 
   def test_inject_will_blow_your_mind
-    result = [2, 3, 4].inject(0) do |sum, item| 
-      puts  sum + item 
-    end
-    assert_equal __, result
+    result = [2, 3, 4].inject(0) { |sum, item| sum + item }
+    assert_equal 9, result
 
-    result2 = [2, 3, 4].inject(1) { |sum, item| sum * item }
-    assert_equal __, result2
+    result2 = [2, 3, 4].inject(1) { |accumulator, item| accumulator * item }
+    assert_equal 24, result2
 
     # Extra Credit:
     # Describe in your own words what inject does.
@@ -77,12 +75,12 @@ class AboutIteration < EdgeCase::Koan
   def test_all_iteration_methods_work_on_any_collection_not_just_arrays
     # Ranges act like a collection
     result = (1..3).map { |item| item + 10 }
-    assert_equal __, result
+    assert_equal [11,12,13], result
 
     # Files act like a collection of lines
     file = File.open("example_file.txt")
     upcase_lines = file.map { |line| line.strip.upcase }
-    assert_equal __, upcase_lines
+    assert_equal ["THIS", "IS", "A", "TEST"], upcase_lines
 
     # NOTE: You can create your own collections that work with each,
     # map, select, etc.
